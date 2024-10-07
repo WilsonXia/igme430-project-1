c79b01bbd572a43188cf02899b550cb45c4e4d44
WilsonXia <wx4477@rit.edu>
2024-10-07 13:16:49 -0400

    set upu web dev tools, webpack and nodemon

--- src/httpResponses.js
+++ src/httpResponses.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 const css = fs.readFileSync(`${__dirname}/../client/style.css`);
-const js = fs.readFileSync(`${__dirname}/../client/client.js`);
+const js = fs.readFileSync(`${__dirname}/../hosted/bundle.js`);
 
 const getIndex = (request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/html' });
